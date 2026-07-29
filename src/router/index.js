f31f1bdaad60438eecde3e16b2f9d210186da440
zakkdruzer <zakkdruzer@gmail.com>
2026-07-29 20:46:10 -0300

    feat: mods para ver en gitpages

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import NuevoTicketView from '../views/NuevoTicketView.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView,
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: LoginView,
-  },
-  {
-    path: '/nuevo-ticket',
-    name: 'nuevo-ticket',
-    component: NuevoTicketView,
-    beforeEnter: () => {
-      const token = localStorage.getItem('token')
-      return token ? true : '/login'
-    },
-  },
-]
-
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+  history: createWebHashHistory(),
+  routes: [
+    { path: '/', name: 'home', component: HomeView },
+    { path: '/login', name: 'login', component: LoginView },
+    { path: '/nuevo-ticket', name: 'nuevo-ticket', component: NuevoTicketView },
+  ],
 })
 
 export default router
