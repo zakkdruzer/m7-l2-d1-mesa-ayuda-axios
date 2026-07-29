8cebd3095e228585080de1ce8136bd59ce96b03e
zakkdruzer <zakkdruzer@gmail.com>
2026-07-28 23:40:15 -0300

    feat: implementar login con jwt

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
+import NuevoTicketView from '@/views/NuevoTicketView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView,
+  },
+  {
+    path: '/nuevo-ticket',
+    name: 'nuevo-ticket',
+    component: NuevoTicketView,
+    beforeEnter: (to, from, next) => {
+      const token = localStorage.getItem('token')
+      token ? next() : next('/login')
+    },
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
